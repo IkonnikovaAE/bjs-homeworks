48ef8c2e6f9bc5056f7568ac645cf64261b338f1
IkonnikovaAE <lisa_dilan@mail.ru>
2019-02-25 21:09:20 +1000

    Добавлено обновление Main.js

---
+++ variables/main.js
@@ -0,0 +1,18 @@
+//Задание 1
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1
+console.log(result);
+
+//Задание 2
+let algebra = 5;
+let gegraphy = 4;
+let phisics = 4;
+let middlePoint = (algebra + gegraphy + phisics) / 3
+console.log('Средняя оценка ученика по предметам: ' + middlePoint);
+
+//Задача 3
+let name = 'Анастасия';
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
