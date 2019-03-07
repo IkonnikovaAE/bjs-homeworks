e826a8d6585002b5718adb3151bd3755cd3c4034
IkonnikovaAE <lisa_dilan@mail.ru>
2019-03-07 21:00:16 +1000

    first commit object-prototype

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -8,6 +8,14 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
+    birthday = new Date(birthday);
+    let date = new Date();
+    let diff = date.getTime() - birthday.getTime();
+    let age = diff / (3,154e+10 + 2,16e+7);
+    if (age >= 18) {
+    	return "да"
+    }
+
 }
 
 function initPrintAnimalSound() {
@@ -22,6 +30,12 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
+    let sound = animal.sound;
+    if (animal = undefined) {
+    	return null;
+    } else {
+    	return sound;
+    }
 }
 
 function initCalculateStatement() {
@@ -36,4 +50,11 @@ function initCalculateStatement() {
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
+    let average = 0;
+    for (let i=0; i < marks.length; i++) {
+    	average = average + parseInt(marks[i]);
+    }
+    average = average / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
